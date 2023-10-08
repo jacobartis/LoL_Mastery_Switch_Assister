7d4730e132d8057218a2d868ad4ef2306ee47796
jacobartis <ZJAC219@live.rhul.ac.uk>
2023-10-08 15:16:43 +0100

    Implemented show_img and show_as_img commands by stitching the given champ icons together and sening as one image file. Allowed mastery to be set to 0 and added error mesage if tying to set below 0.

--- msa_discord_bot.py
+++ msa_discord_bot.py
@@ -1,6 +1,9 @@
 import discord
 import json
 from PIL import Image 
+from io import BytesIO
+from os import remove
+from math import ceil
 from os.path import exists
 from discord.utils import get
 from discord.ext import commands
@@ -148,12 +151,15 @@ async def enable_all_summoners_error(ctx, error):
 @bot.command(name="mastery",help = "Sets the mastery value of the pool.")
 async def set_mastery(ctx, value):
     try:
-        assert(int(value))
+        assert(int(value)+1)
         champ_pool.set_mastery(int(value))
         await ctx.channel.send("**Set mastery to:** "+value)
         save_file()
     except Exception as e:
-        await ctx.channel.send("Error setting mastery: "+str(e))
+        if int(value)<0:
+            await ctx.channel.send("Error setting mastery: "+"value must be 0 or higher")
+        else:
+            await ctx.channel.send("Error setting mastery: "+str(e))
 
 #command to show the pool mastery value
 @set_mastery.error
@@ -196,27 +202,75 @@ async def show_pool_error(ctx, error):
 #command to show the current pool
 @commands.bot_has_permissions(send_messages=True,read_messages=True)
 @bot.command(name="show_img",help = "Shows icons of current pool.")
-async def show_pool_img(ctx):
+async def show_pool_img(ctx,names):
     try:
-        await ctx.channel.send("__**Current pool:**__ \n")
-        for url in champ_pool.get_pool_img():
-            await ctx.channel.send(url)
-#        for byts in champ_pool.get_pool_img():
-#            img = Image.open(byts.read())
-#            img.save(byts,format='PNG')
-#            img.read()
-#            byts.seek(0)
-#            byts.read()
-#            await ctx.channel.send(discord.File(img))
+        assert(str(names))
+        names_list = names.split()
+        await ctx.channel.send("__**Champ pool for summoner(s) "+str(names)+":**__ \n")
+        for name in names_list:
+
+            champs = msa.Summoner(name,msa.get_champ_data(),riot_key).get_champs_over_value(champ_pool.mastery)
+            
+            #Creates a blank image of the required width and height
+            img_size = 120
+            width = 6
+            height = ceil(len(champs)/width)
+            pool_img = Image.new("RGB",[img_size*width,img_size*height])
+
+            cur_width = 0
+            cur_height = 0
+            
+            
+
+            #Itterates through the champs and stiched all icons together
+            for byts in msa.get_champ_icons(champs):
+                byte_io = BytesIO(byts)
+                new_icon = Image.open(byte_io)
+                pool_img.paste(new_icon,[img_size*cur_width,img_size*cur_height])
+
+                cur_width = (cur_width+1)%width
+                if cur_width == 0:
+                    cur_height += 1
+
+            #Saves the new image, sends it, then deletes it.
+            pool_img.save("champ_pool.png")
+            await ctx.channel.send("**"+name+"**")
+            await ctx.channel.send(file=discord.File("champ_pool.png"))
+            remove("champ_pool.png")
+
     except Exception as e:
         await ctx.channel.send("Error showing pool images: "+str(e))
 
-#Handles errors for showing the pool
+#Handles errors for showing the pool as img
 @show_pool_img.error
 async def show_pool_img(ctx, error):
     try:
+        #if no input, print all
         if isinstance(error, commands.MissingRequiredArgument):
-            await ctx.channel.send("__**Current pool:**__ \n"+"\n".join(champ_pool.get_pool()))
+            #Creates a blank image of the required width and height
+            img_size = 120
+            width = 6
+            height = ceil(len(champ_pool.get_pool())/width)
+            pool_img = Image.new("RGB",[img_size*width,img_size*height])
+
+            cur_width = 0
+            cur_height = 0
+
+            #Itterates through the champs and stiched all icons together
+            for byts in msa.get_champ_icons(champ_pool.get_pool()):
+                byte_io = BytesIO(byts)
+                new_icon = Image.open(byte_io)
+                pool_img.paste(new_icon,[img_size*cur_width,img_size*cur_height])
+
+                cur_width = (cur_width+1)%width
+                if cur_width == 0:
+                    cur_height += 1
+
+            #Saves the new image, sends it, then deletes it.
+            pool_img.save("champ_pool.png")
+            await ctx.channel.send("__**Current pool:**__ \n")
+            await ctx.channel.send(file=discord.File("champ_pool.png"))
+            remove("champ_pool.png")
     except Exception as e:
         await ctx.channel.send("Error showing champ pool as images: "+str(e))
 
@@ -245,6 +299,57 @@ async def show_pool_as_error(ctx, error):
             await ctx.channel.send("Please include a summoner name to show pool as.")
     except Exception as e:
         await ctx.channel.send("Error showing champ pool as error : "+str(e))
+
+
+#command to show the current pool as a summoner using images
+@commands.bot_has_permissions(send_messages=True,read_messages=True)
+@bot.command(name="show_as_img",help = "Shows the current pool as a summoner in an image (removes their champs from the pool).")
+async def show_pool_as(ctx,names):
+    try:
+        assert(str(names))
+        summoners = []
+        names_list = names.split()
+        for name in names_list:
+            summoner = msa.Summoner(name,msa.get_champ_data(),riot_key)
+            assert(summoner != None)
+            summoners.append(summoner)
+        
+        #Creates a blank image of the required width and height
+        img_size = 120
+        width = 6
+        height = ceil(len(champ_pool.get_pool_as(summoners))/width)
+        pool_img = Image.new("RGB",[img_size*width,img_size*height])
+
+        cur_width = 0
+        cur_height = 0
+
+        #Itterates through the champs and stiched all icons together
+        for byts in msa.get_champ_icons(champ_pool.get_pool_as(summoners)):
+            byte_io = BytesIO(byts)
+            new_icon = Image.open(byte_io)
+            pool_img.paste(new_icon,[img_size*cur_width,img_size*cur_height])
+
+            cur_width = (cur_width+1)%width
+            if cur_width == 0:
+                cur_height += 1
+
+        #Saves the new image, sends it, then deletes it.
+        pool_img.save("champ_pool.png")
+        await ctx.channel.send("__**Current pool:**__ \n")
+        await ctx.channel.send(file=discord.File("champ_pool.png"))
+        remove("champ_pool.png")
+        
+    except Exception as e:
+        await ctx.channel.send("Error showing as: "+str(e))
+
+#Handles errors for showing the pool as a summoner
+@show_pool_as.error
+async def show_pool_as_error(ctx, error):
+    try:
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.channel.send("Please include a summoner name to show pool as image.")
+    except Exception as e:
+        await ctx.channel.send("Error showing champ pool as image error : "+str(e))
 
 
 #command to show all summoners 
